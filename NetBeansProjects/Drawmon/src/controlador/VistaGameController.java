01c1adaebef41222e3ab86ef8ef05eb47f168db7
jczr8 <jczr8@LAPTOP-4M5FRU1N>
2023-08-04 11:11:58 +0200

    4-8-23 1111

--- NetBeansProjects/Drawmon/src/controlador/VistaGameController.java
+++ NetBeansProjects/Drawmon/src/controlador/VistaGameController.java
@@ -1,5 +1,10 @@
 package controlador;
 
+import java.util.Random;
+import javafx.animation.KeyFrame;
+import javafx.animation.RotateTransition;
+import javafx.animation.ScaleTransition;
+import javafx.animation.Timeline;
 import javafx.animation.TranslateTransition;
 import javafx.fxml.FXML;
 import javafx.scene.image.Image;
@@ -24,11 +29,56 @@ public class VistaGameController {
         imageViewFondo.setFitHeight(720);
         // Animacion mascota
 
-        TranslateTransition tt = new TranslateTransition(Duration.millis(2000), mascota);
-        tt.setByX(100);
-        tt.setCycleCount(TranslateTransition.INDEFINITE);
-        tt.setAutoReverse(true);
-        tt.play();
-    }
+        TranslateTransition tt1 = new TranslateTransition(Duration.millis(2000), mascota);
+        tt1.setByX(100);
+        tt1.setCycleCount(1);
+        tt1.setAutoReverse(true);
 
+        // Crea una transición de escala para aplastar y estirar la imagen
+        ScaleTransition st = new ScaleTransition(Duration.millis(100), mascota);
+        st.setByY(-0.2);
+        st.setCycleCount(2);
+        st.setAutoReverse(true);
+
+// Crea una transición de rotación para hacer vibrar la imagen
+        RotateTransition rt = new RotateTransition(Duration.millis(50), mascota);
+        rt.setByAngle(10);
+        rt.setCycleCount(8);
+        rt.setAutoReverse(true);
+
+// Crea una transición de traslación para hacer saltar la imagen
+        TranslateTransition tt = new TranslateTransition(Duration.millis(200), mascota);
+        tt.setByY(-50);
+        tt.setCycleCount(2);
+        tt.setAutoReverse(true);
+
+        // Combina las transiciones en una secuencia
+     
+
+        Timeline timeline = new Timeline(new KeyFrame(Duration.seconds(5), e -> {
+            // Select a random animation
+            int rand = new Random().nextInt(4);
+            switch (rand) {
+                case 0:
+                    tt.play();
+                    break;
+                case 1:
+                    st.play();
+                    break;
+                case 2:
+                    rt.play();
+                    break;
+                case 3:
+                    tt1.play();
+                    break;
+                   
+            }
+        }));
+        
+        
+        timeline.setCycleCount(Timeline.INDEFINITE);
+        timeline.play();
+    }
 }
+
+
